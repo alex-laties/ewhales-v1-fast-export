93655569833abc1ab1f03b4934128722908dfefa
Alexander Laties <alex@laties.info>
2026-06-12 16:12:37 -0400

    forgot to include test file

---
+++ test/test_simple.sql
@@ -0,0 +1,5 @@
+CREATE TABLE logswp_postmeta (meta_id INT, post_id INT, meta_key VARCHAR, meta_value VARCHAR);
+INSERT INTO logswp_postmeta (meta_id, post_id, meta_key, meta_value) VALUES (1, 100, 'logbook_id', 'Test Logbook 1888');
+INSERT INTO logswp_postmeta (meta_id, post_id, meta_key, meta_value) VALUES (2, 200, 'logbook_id', '100');
+INSERT INTO logswp_postmeta (meta_id, post_id, meta_key, meta_value) VALUES (3, 200, 'cloud_cover', 'Partly Cloudy');
+INSERT INTO logswp_postmeta (meta_id, post_id, meta_key, meta_value) VALUES (4, 200, 'sea_state', 'Calm');
